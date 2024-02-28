218e4a82b83031b32a09cd9ad8115fc4d916caa4
Rafael Moreno <rafaelmorenosax@gmail.com>
2024-02-27 22:32:08 -0300

    Aula 2

--- Aula 2/app/src/main/java/com/example/aula2/MainActivity.java
+++ Aula 2/app/src/main/java/com/example/aula2/MainActivity.java
@@ -16,7 +16,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     Spinner spinnerSexo;
-    EditText textIdade;
+    EditText textIdade, textValorAutomovel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
 
         spinnerSexo = (Spinner) findViewById(R.id.spinnerSexo);
         textIdade = (EditText) findViewById(R.id.textIdade);
+        textValorAutomovel = (EditText) findViewById(R.id.textValorAutomovel);
 
         List<String> opcoesSexo = Arrays.asList("Masculino","Feminino");
         ArrayAdapter<String> adapterSexo =
@@ -50,10 +51,15 @@ public class MainActivity extends AppCompatActivity {
     public void buttonCalcularClick(View v){
         String sexo = spinnerSexo.getSelectedItem().toString();
         int idade = Integer.parseInt(textIdade.getText().toString());
+        double valorAutomovel = Double.parseDouble(textValorAutomovel.getText().toString());
         double valorApolice = 0;
 
         if(sexo.equals("Masculino") && idade >= 35)
-            valorApolice = 0;
+            valorApolice = valorAutomovel * 10/100;
+        if(sexo.equals("Masculino") && idade < 35)
+            valorApolice = valorAutomovel * 5/100;
+        if(sexo.equals("Feminino"))
+            valorApolice = valorAutomovel * 2/100;
 
         Toast.makeText(this,
          "O valor é: " + valorApolice,Toast.LENGTH_SHORT).show();
